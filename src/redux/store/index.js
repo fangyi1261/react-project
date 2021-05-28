06c453f5c386dca127f44fc4d3bda53b9dbf53b3
fangyi <fangyifangyif@126.com>
2021-05-28 09:10:15 +0800

    新增redux-devtools-extension浏览器查看状态插件

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,13 +5,14 @@
 import { createStore, applyMiddleware } from 'redux'; // 引入 createStore applyMiddleware方法
 import reducer from '@/redux/reducer';                 // 引入reducer
 import thunk from 'redux-thunk';                      // 引入thunk
+import { composeWithDevTools } from 'redux-devtools-extension'; // 打开react-redux-devtools
 
 // 创建数据存储仓库
 const store = createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 );
-// 使浏览器中redux-devtool插件生效
+// 未使用applyMiddleware,使浏览器中redux-devtool插件生效
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export default store; // 将仓库暴露出去

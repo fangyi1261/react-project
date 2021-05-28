357292eb04274558cef73e239628da1c30c483cf
fangyi <fangyifangyif@126.com>
2021-05-28 16:44:28 +0800

    调试工具区分环境

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -7,10 +7,17 @@ import reducer from '@/redux/reducer';                 // 引入reducer
 import thunk from 'redux-thunk';                      // 引入thunk
 import { composeWithDevTools } from 'redux-devtools-extension'; // 打开react-redux-devtools
 
+let enhancers = null;
+if(process.env.NODE_ENV === 'development') {
+  enhancers = composeWithDevTools(applyMiddleware(thunk));
+} else {
+  enhancers = applyMiddleware(thunk);
+}
+
 // 创建数据存储仓库
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  enhancers
 );
 // 未使用applyMiddleware,使浏览器中redux-devtool插件生效
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
